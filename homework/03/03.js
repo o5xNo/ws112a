ace6fa688b6a82997aa850172623a1e6f5dbab9b
o5xNo <asd38894268@gmail.com>
2023-09-23 14:57:43 +0800

    re

--- homework/03/03.js
+++ homework/03/03.js
@@ -1,52 +1,79 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
-import * as render from './render.js'
+import * as render from './render.js';
 
 const posts = [
-  {id:0, title:'孫悟空', body:'0909090909'},
-  {id:1, title:'豬八戒', body:'0988888888'}
+  { id: 0, title: '孫悟空', body: '0909090909' },
+  { id: 1, title: '豬八戒', body: '0988888888' },
 ];
 
 const router = new Router();
 
-router.get('/', list)
-  .get('/person/new', add)
-  .get('/person/:id', show)
-  .post('/person', create);
+router
+  .get('/', list)
+  .get('/post/new', add)
+  .get('/post/:id', show)
+  .post('/post', create)
+  .get('/search', searchForm)
+  .post('/search', search);
 
-  const app = new Application();
-  app.use(router.routes());
-  app.use(router.allowedMethods());
-  
-  async function list(ctx) {
-    ctx.response.body = await render.list(posts);
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+async function list(ctx) {
+  ctx.response.body = await render.list(posts);
+}
+
+async function add(ctx) {
+  const content = await render.newPost() + render.searchForm();
+  ctx.response.body = content;
+}
+
+async function show(ctx) {
+  const id = ctx.params.id;
+  const post = posts[id];
+  if (!post) ctx.throw(404, 'invalid post id');
+  ctx.response.body = await render.show(post);
+}
+
+async function create(ctx) {
+  const body = ctx.request.body();
+  if (body.type === "form") {
+    const pairs = await body.value;
+    const post = {};
+    for (const [key, value] of pairs) {
+      post[key] = value;
+    }
+    console.log('post=', post);
+    const id = posts.push(post) - 1;
+    post.created_at = new Date();
+    post.id = id;
+    ctx.response.redirect('/');
   }
-  
-  async function add(ctx) {
-    ctx.response.body = await render.newPost();
-  }
-  
-  async function show(ctx) {
-    const id = ctx.params.id;
-    const post = posts[id];
-    if (!post) ctx.throw(404, 'invalid post id');
-    ctx.response.body = await render.show(post);
-  }
-  
-  async function create(ctx) {
-    const body = ctx.request.body()
-    if (body.type === "form") {
-      const pairs = await body.value
-      const post = {}
-      for (const [key, value] of pairs) {
-        post[key] = value
-      }
-      console.log('post=', post)
-      const id = posts.push(post) - 1;
-      post.created_at = new Date();
-      post.id = id;
-      ctx.response.redirect('/');
+}
+
+function searchForm(ctx) {
+  ctx.response.body = render.searchForm();
+}
+
+async function search(ctx) {
+  const body = ctx.request.body();
+  if (body.type === "form") {
+    const pairs = await body.value;
+    const searchName = pairs.get('name');
+    const result = searchByName(searchName);
+    if (result) {
+      ctx.response.body = render.show(result.title, result.body);
+    } else {
+      ctx.response.body = '查無此人';
     }
   }
-  
-  console.log('Server run at http://127.0.0.1:8000')
-  await app.listen({ port: 8000 });
+}
+
+function searchByName(name) {
+  const foundPost = posts.find(post => post.title === name);
+  return foundPost ? { title: foundPost.title, body: foundPost.body } : null;
+}
+
+console.log('Server run at http://127.0.0.1:8000');
+await app.listen({ port: 8000 });
--- homework/03/render.js
+++ homework/03/render.js
@@ -1,105 +1,121 @@
 export function layout(title, content) {
-    return `
-    <html>
-    <head>
-      <title>${title}</title>
-      <style>
-        body {
-          padding: 80px;
-          font: 16px Helvetica, Arial;
-        }
-    
-        h1 {
-          font-size: 2em;
-        }
-    
-        h2 {
-          font-size: 1.2em;
-        }
-    
-        #posts {
-          margin: 0;
-          padding: 0;
-        }
-    
-        #posts li {
-          margin: 40px 0;
-          padding: 0;
-          padding-bottom: 20px;
-          border-bottom: 1px solid #eee;
-          list-style: none;
-        }
-    
-        #posts li:last-child {
-          border-bottom: none;
-        }
-    
-        textarea {
-          width: 500px;
-          height: 300px;
-        }
-    
-        input[type=text],
-        textarea {
-          border: 1px solid #eee;
-          border-top-color: #ddd;
-          border-left-color: #ddd;
-          border-radius: 2px;
-          padding: 15px;
-          font-size: .8em;
-        }
-    
-        input[type=text] {
-          width: 500px;
-        }
-      </style>
-    </head>
-    <body>
-      <section id="content">
-        ${content}
-      </section>
-    </body>
-    </html>
-    `
-  }
+  return `
+  <html>
+  <head>
+    <title>${title}</title>
+    <style>
+      body {
+        padding: 80px;
+        font: 16px Helvetica, Arial;
+      }
   
-  export function list(posts) {
-    let list = []
-    for (let post of posts) {
-      list.push(`
-      <li>
-        <h2>${ post.title }</h2>
-        <p><a href="/post/${post.id}">Read post</a></p>
-      </li>
-      `)
-    }
-    let content = `
-    <h1>聯絡人</h1>
-    <p>您有<strong>${ppeople.length}</strong> 個聯絡人!</p>
-    <p><a href="/post/new">新增聯絡人</a></p>
-    <ul id="posts">
-      ${list.join('\n')}
-    </ul>
-    `
-    return layout('People', content)
-  }
+      h1 {
+        font-size: 2em;
+      }
   
-  export function newPost() {
-    return layout('新聯絡人', `
-    <h1>新聯絡人</h1>
-    <p>輸入新聯絡人的資料</p>
-    <form action="/person" method="person">
-      <p><input type="text" placeholder="姓名" name="title"></p>
-      <p><textarea placeholder="電話" name="body"></textarea></p>
-      <p><input type="submit" value="新增"></p>
+      h2 {
+        font-size: 1.2em;
+      }
+  
+      #posts {
+        margin: 0;
+        padding: 0;
+      }
+  
+      #posts li {
+        margin: 40px 0;
+        padding: 0;
+        padding-bottom: 20px;
+        border-bottom: 1px solid #eee;
+        list-style: none;
+      }
+  
+      #posts li:last-child {
+        border-bottom: none;
+      }
+  
+      textarea {
+        width: 500px;
+        height: 300px;
+      }
+  
+      input[type=text],
+      textarea {
+        border: 1px solid #eee;
+        border-top-color: #ddd;
+        border-left-color: #ddd;
+        border-radius: 2px;
+        padding: 15px;
+        font-size: .8em;
+      }
+  
+      input[type=text] {
+        width: 500px;
+      }
+    </style>
+  </head>
+  <body>
+    <section id="content">
+      ${content}
+    </section>
+  </body>
+  </html>
+  `;
+}
+
+export function list(posts) {
+  let listItems = [];
+  for (let post of posts) {
+    listItems.push(`
+    <li>
+      <h2>${post.title}</h2>
+      <p><a href="/post/${post.id}">詳細資料</a></p>
+    </li>
+    `);
+  }
+  let content = `
+  <h1>聯絡人</h1>
+  <p>您有<strong>${posts.length}</strong> 個聯絡人!</p>
+  <p><a href="/post/new">新增聯絡人</a></p>
+  <p><a href="/search">查詢</a></p>
+  <ul id="posts">
+    ${listItems.join('\n')}
+  </ul>
+  `;
+  return layout('Posts', content);
+}
+
+export function newPost() {
+  return layout('New Post', `
+  <h1>新聯絡人</h1>
+  <p>輸入新聯絡人的資料</p>
+  <form action="/post" method="post">
+    <p><input type="text" placeholder="姓名" name="title"></p>
+    <p><textarea placeholder="電話" name="body"></textarea></p>
+    <p><input type="submit" value="新增"></p>
+  </form>
+  `);
+}
+export function searchForm() {
+  return layout('Search', `
+    <h1>搜索联系人</h1>
+    <form action="/search" method="post">
+      <p><input type="text" placeholder="姓名" name="name"></p>
+      <p><input type="submit" value="搜索"></p>
     </form>
-    `)
+  `);
+}
+
+export function show(titleOrPost, body) {
+  if (typeof titleOrPost === 'string') {
+    return layout(titleOrPost, `
+      <h1>${titleOrPost}</h1>
+      <p>${body}</p>
+    `);
+  } else if (typeof titleOrPost === 'object' && titleOrPost !== null) {
+    return layout(titleOrPost.title, `
+      <h1>${titleOrPost.title}</h1>
+      <pre>${titleOrPost.body}</pre>
+    `);
   }
-  
-  export function show(post) {
-    return layout(post.title, `
-      <h1>${post.title}</h1>
-      <pre>${post.body}</pre>
-    `)
-  }
-  
+}
