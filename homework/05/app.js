35b07f9c4fdaab9ffe105bef8ed8934c8fbc2f61
o5xNo <asd38894268@gmail.com>
2023-10-20 00:04:48 +0800

    re

--- homework/05/app.js
+++ homework/05/app.js
@@ -1,5 +1,5 @@
 import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
-import * as render from './render.js'
+import * as render from './render.js';
 import { DB } from "https://deno.land/x/sqlite/mod.ts";
 
 const db = new DB("blog.db");
@@ -17,9 +17,7 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 
 app.use(async (ctx) => {
-  console.log('path=', ctx.request.url.pathname)
   if (ctx.request.url.pathname.startsWith("/public/")) {
-    console.log('pass:', ctx.request.url.pathname)
     await send(ctx, ctx.request.url.pathname, {
       root: Deno.cwd(),
       index: "index.html",
@@ -30,14 +28,13 @@ app.use(async (ctx) => {
 function query(sql) {
   let list = []
   for (const [id, title, body] of db.query(sql)) {
-    list.push({id, title, body})
+    list.push({ id, title, body })
   }
   return list
 }
 
 async function list(ctx) {
   let posts = query("SELECT id, title, body FROM posts")
-  console.log('list:posts=', posts)
   ctx.response.body = await render.list(posts);
 }
 
@@ -49,7 +46,6 @@ async function show(ctx) {
   const pid = ctx.params.id;
   let posts = query(`SELECT id, title, body FROM posts WHERE id=${pid}`)
   let post = posts[0]
-  console.log('show:post=', post)
   if (!post) ctx.throw(404, 'invalid post id');
   ctx.response.body = await render.show(post);
 }
@@ -62,11 +58,10 @@ async function create(ctx) {
     for (const [key, value] of pairs) {
       post[key] = value
     }
-    console.log('create:post=', post)
     db.query("INSERT INTO posts (title, body) VALUES (?, ?)", [post.title, post.body]);
     ctx.response.redirect('/');
   }
 }
 
-console.log('Server run at http://127.0.0.1:8000')
+console.log('Server run at http://127.0.0.1:8000');
 await app.listen({ port: 8000 });
